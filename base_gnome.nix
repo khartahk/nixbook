50df55eb23a80fae508a50af997b157782420833
khartahk <khartahk@users.noreply.github.com>
2025-11-01 10:55:19 +0100

    feat: add gnome desktop

--- base_gnome.nix
+++ base_gnome.nix
@@ -1,10 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    ./base.nix
-  ];
-
   services.xserver.displayManager.lightdm.enable = lib.mkForce false;
   services.xserver.desktopManager.cinnamon.enable = lib.mkForce false;
 
